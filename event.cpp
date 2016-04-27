4aab8b515a091eee8e75c7aadffe23bf174d4980
tclark55 <tclark55@uncc.edu>
2016-04-27 10:18:17 -0400

    Update event.cpp

--- event.cpp
+++ event.cpp
@@ -4,12 +4,21 @@
 void check(){
 
   //function to check if there is an event at players current location
-
+  int playLoc = p1.getLoc();
+  //check if theres an event at that position
+    for(read event database)
+    //get the location even number
+    if (playLoc == eventNum){
+      //if there is call the event handler function
+    }
+    
 }
 
 void handle(){
 
   //if there is an event at players location
+   int playLoc = p1.getLoc();
+  //get the event
   //output event/question
   //if event change user info
   //if question, change user info based on answer
